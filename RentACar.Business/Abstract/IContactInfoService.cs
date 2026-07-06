0639cb26a53dead499c2567a58530df2936b1c39
bugrabalim11 <bugrablm11@gmail.com>
2026-07-06 22:42:32 +0300

    feat: Temel iş kurallarıyla birlikte IContactInfoService ve ContactInfoManager eklendi.

---
+++ RentACar.Business/Abstract/IContactInfoService.cs
@@ -0,0 +1,16 @@
+﻿using RentACar.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Business.Abstract
+{
+    public interface IContactInfoService
+    {
+        Task<List<ContactInfo>> GetAllAsync();
+        Task<ContactInfo?> GetByIdAsync(int id);
+        Task AddAsync(ContactInfo contactInfo);
+        Task UpdateAsync(ContactInfo contactInfo);
+        Task DeleteAsync(ContactInfo contactInfo);
+    }
+}
---
+++ RentACar.Business/Concrete/ContactInfoManager.cs
@@ -0,0 +1,44 @@
+﻿using RentACar.Business.Abstract;
+using RentACar.DataAccess.Abstract;
+using RentACar.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Business.Concrete
+{
+    public class ContactInfoManager : IContactInfoService
+    {
+        private readonly IRepository<ContactInfo> _contactInfoRepository;
+
+        public ContactInfoManager(IRepository<ContactInfo> contactInfoRepository)
+        {
+            _contactInfoRepository = contactInfoRepository;
+        }
+
+        public async Task AddAsync(ContactInfo contactInfo)
+        {
+            await _contactInfoRepository.AddAsync(contactInfo);
+        }
+
+        public async Task DeleteAsync(ContactInfo contactInfo)
+        {
+            await _contactInfoRepository.DeleteAsync(contactInfo);
+        }
+
+        public async Task<List<ContactInfo>> GetAllAsync()
+        {
+            return await _contactInfoRepository.GetAllAsync();
+        }
+
+        public Task<ContactInfo?> GetByIdAsync(int id)
+        {
+            return _contactInfoRepository.GetAsync(x => x.Id == id);
+        }
+
+        public async Task UpdateAsync(ContactInfo contactInfo)
+        {
+            await _contactInfoRepository.UpdateAsync(contactInfo);
+        }
+    }
+}
