3ea821cf61de2b02742dc77954c2841456447e9f
bugrabalim11 <bugrablm11@gmail.com>
2026-07-06 22:17:42 +0300

    feat: Temel iş kurallarıyla birlikte IRentalService ve RentalManager eklendi.

---
+++ RentACar.Business/Abstract/IRentalService.cs
@@ -0,0 +1,16 @@
+﻿using RentACar.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Business.Abstract
+{
+    public interface IRentalService
+    {
+        Task<List<Rental>> GetAllAsync();
+        Task<Rental?> GetByIdAsync(int id);
+        Task AddAsync(Rental rental);
+        Task UpdateAsync(Rental rental);
+        Task DeleteAsync(Rental rental);
+    }
+}
---
+++ RentACar.Business/Concrete/RentalManager.cs
@@ -0,0 +1,54 @@
+﻿using RentACar.Business.Abstract;
+using RentACar.DataAccess.Abstract;
+using RentACar.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        private readonly IRepository<Rental> _rentalRepository;
+
+        public RentalManager(IRepository<Rental> rentalRepository)
+        {
+            _rentalRepository = rentalRepository;
+        }
+
+        public async Task AddAsync(Rental rental)
+        {
+            // İŞ KURALI (BUSINESS RULE): Araba şu an müsait mi?
+            // Veritabanına gidip soruyoruz: "Bu CarId'ye sahip ve ReturnDate'i (Dönüş Tarihi) NULL olan bir kayıt var mı?"
+            var isCarRented = await _rentalRepository.GetAsync(r => r.CarId == rental.CarId && r.ReturnDate == null);
+
+            // Eğer isCarRented değişkeni 'null' DÖNMEDİYSE (yani içerisi doluysa), demek ki araba şu an dışarıda!
+            if (isCarRented != null) // != Eşit değilse demek
+            {
+                throw new Exception("Bu araç şu an başka bir müşteride kiralık durumda. Garaja dönmeden tekrar kiralanamaz!");
+            }
+
+            await _rentalRepository.AddAsync(rental);
+        }
+
+        public async Task DeleteAsync(Rental rental)
+        {
+            await _rentalRepository.DeleteAsync(rental);
+        }
+
+        public async Task<List<Rental>> GetAllAsync()
+        {
+            return await _rentalRepository.GetAllAsync();
+        }
+
+        public async Task<Rental?> GetByIdAsync(int id)
+        {
+            return await _rentalRepository.GetAsync(x => x.Id == id);
+        }
+
+        public async Task UpdateAsync(Rental rental)
+        {
+            await _rentalRepository.UpdateAsync(rental);
+        }
+    }
+}
