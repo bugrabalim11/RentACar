7ab437f01722442788313fa4e9727157ce9d23b5
bugrabalim11 <bugrablm11@gmail.com>
2026-07-06 22:50:28 +0300

    feat: Temel iş kurallarıyla birlikte IContactMessageService ve ContactMessageManager eklendi.

---
+++ RentACar.Business/Abstract/IContactMessageService.cs
@@ -0,0 +1,16 @@
+﻿using RentACar.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Business.Abstract
+{
+    public interface IContactMessageService
+    {
+        Task<List<ContactMessage>> GetAllAsync();
+        Task<ContactMessage?> GetByIdAsync(int id);
+        Task AddAsync(ContactMessage contactMessage);
+        Task UpdateAsync(ContactMessage contactMessage);
+        Task DeleteAsync(ContactMessage contactMessage);
+    }
+}
---
+++ RentACar.Business/Concrete/ContactMessageManager.cs
@@ -0,0 +1,44 @@
+﻿using RentACar.Business.Abstract;
+using RentACar.DataAccess.Abstract;
+using RentACar.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Business.Concrete
+{
+    public class ContactMessageManager : IContactMessageService
+    {
+        private readonly IRepository<ContactMessage> _contactMessageRepository;
+
+        public ContactMessageManager(IRepository<ContactMessage> contactMessageRepository)
+        {
+            _contactMessageRepository = contactMessageRepository;
+        }
+
+        public async Task AddAsync(ContactMessage contactMessage)
+        {
+            await _contactMessageRepository.AddAsync(contactMessage);
+        }
+
+        public async Task DeleteAsync(ContactMessage contactMessage)
+        {
+            await _contactMessageRepository.DeleteAsync(contactMessage);
+        }
+
+        public async Task<List<ContactMessage>> GetAllAsync()
+        {
+            return await _contactMessageRepository.GetAllAsync();
+        }
+
+        public async Task<ContactMessage?> GetByIdAsync(int id)
+        {
+            return await _contactMessageRepository.GetAsync(x => x.Id == id);
+        }
+
+        public async Task UpdateAsync(ContactMessage contactMessage)
+        {
+            await _contactMessageRepository.UpdateAsync(contactMessage);
+        }
+    }
+}
