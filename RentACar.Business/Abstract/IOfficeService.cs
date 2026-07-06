9d4d0a2060acda2ae2ae7e8afdd82760ecaa1605
bugrabalim11 <bugrablm11@gmail.com>
2026-07-06 22:33:58 +0300

    feat: Temel iş kurallarıyla birlikte IOfficeService ve OfficeManager eklendi.

---
+++ RentACar.Business/Abstract/IOfficeService.cs
@@ -0,0 +1,16 @@
+﻿using RentACar.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Business.Abstract
+{
+    public interface IOfficeService
+    {
+        Task<List<Office>> GetAllAsync();
+        Task<Office?> GetByIdAsync(int id);
+        Task AddAsync(Office office);
+        Task UpdateAsync(Office office);
+        Task DeleteAsync(Office office);
+    }
+}
---
+++ RentACar.Business/Concrete/OfficeManager.cs
@@ -0,0 +1,44 @@
+﻿using RentACar.Business.Abstract;
+using RentACar.DataAccess.Abstract;
+using RentACar.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Business.Concrete
+{
+    public class OfficeManager : IOfficeService
+    {
+        private readonly IRepository<Office> _officeRepository;
+
+        public OfficeManager(IRepository<Office> officeRepository)
+        {
+            _officeRepository = officeRepository;
+        }
+
+        public async Task AddAsync(Office office)
+        {
+            await _officeRepository.AddAsync(office);
+        }
+
+        public async Task DeleteAsync(Office office)
+        {
+            await _officeRepository.DeleteAsync(office);
+        }
+
+        public async Task<List<Office>> GetAllAsync()
+        {
+            return await _officeRepository.GetAllAsync();
+        }
+
+        public async Task<Office?> GetByIdAsync(int id)
+        {
+            return await _officeRepository.GetAsync(x => x.Id == id);
+        }
+
+        public async Task UpdateAsync(Office office)
+        {
+            await _officeRepository.UpdateAsync(office);
+        }
+    }
+}
