c84b68cb3709755e80b6e987ebe70b902757bff3
bugrabalim11 <bugrablm11@gmail.com>
2026-07-06 21:27:38 +0300

    feat: Temel iş kurallarıyla birlikte ICarService ve CarManager eklendi.

---
+++ RentACar.Business/Abstract/ICarService.cs
@@ -0,0 +1,16 @@
+﻿using RentACar.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Business.Abstract
+{
+    public interface ICarService
+    {
+        Task<List<Car>> GetAllAsync();
+        Task<Car?> GetByIdAsync(int id); // Bulamazsa null döner (T? mantığı)
+        Task AddAsync(Car car);
+        Task UpdateAsync(Car car);
+        Task DeleteAsync(Car car);
+    }
+}
---
+++ RentACar.Business/Concrete/CarManager.cs
@@ -0,0 +1,52 @@
+﻿using RentACar.Business.Abstract;
+using RentACar.DataAccess.Abstract;
+using RentACar.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        // İşte dün yazdığımız o kusursuz Generic Repository! T yerine Car verdik.
+        private readonly IRepository<Car> _carRepository;
+
+        public CarManager(IRepository<Car> carRepository)
+        {
+            _carRepository = carRepository;
+        }
+
+        public async Task AddAsync(Car car)
+        {
+            if (car.DailyPrice <= 0)
+            {
+                throw new ArgumentException("Günlük araç kiralama bedeli 0'dan büyük olmalıdır.");
+            }
+
+            await _carRepository.AddAsync(car);
+        }
+
+        public async Task DeleteAsync(Car car)
+        {
+            await _carRepository.DeleteAsync(car);
+        }
+
+        public async Task<List<Car>> GetAllAsync()
+        {
+            // İstersek buraya filtre de gönderebilirdik, ama şimdilik hepsini istiyoruz.
+            return await _carRepository.GetAllAsync();
+        }
+
+        public async Task<Car?> GetByIdAsync(int id)
+        {
+            // Dün yazdığımız O sihirli Expression (x => x.Id == id) mantığı burada devreye giriyor!
+            return await _carRepository.GetAsync(x => x.Id == id);
+        }
+
+        public async Task UpdateAsync(Car car)
+        {
+            await _carRepository.UpdateAsync(car);
+        }
+    }
+}
