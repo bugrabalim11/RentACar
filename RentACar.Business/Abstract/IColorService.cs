d50a5f621ed409aa9e528bbacae182d188108c77
bugrabalim11 <bugrablm11@gmail.com>
2026-07-10 10:22:33 +0300

    feat: IColorService ve ColorMnager oluşturuldu.

---
+++ RentACar.Business/Abstract/IColorService.cs
@@ -0,0 +1,17 @@
+﻿using RentACar.Dtos.BrandDtos;
+using RentACar.Dtos.ColorDtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Business.Abstract
+{
+    public interface IColorService
+    {
+        Task<List<ColorListDto>> GetAllAsync();
+        Task<ColorListDto?> GetByIdAsync(int id);
+        Task AddAsync(ColorAddDto colorAddDto);
+        Task<bool> UpdateAsync(ColorUpdateDto colorUpdateDto);
+        Task<bool> DeleteAsync(int id);
+    }
+}
---
+++ RentACar.Business/Concrete/ColorManager.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using RentACar.Business.Abstract;
+using RentACar.DataAccess.Abstract;
+using RentACar.Dtos.ColorDtos;
+using RentACar.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Business.Concrete
+{
+    public class ColorManager : IColorService
+    {
+        private readonly IRepository<Color> _colorRepository;
+        private readonly IMapper _mapper;
+
+        public ColorManager(IRepository<Color> colorRepository, IMapper mapper)
+        {
+            _colorRepository = colorRepository;
+            _mapper = mapper;
+        }
+
+        public async Task AddAsync(ColorAddDto colorAddDto)
+        {
+            var color = _mapper.Map<Color>(colorAddDto);
+            await _colorRepository.AddAsync(color);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var existingColor = await _colorRepository.GetAsync(x => x.Id == id);
+            if (existingColor == null)
+            {
+                return false;
+            }
+            await _colorRepository.DeleteAsync(existingColor);
+            return true;
+        }
+
+        public async Task<List<ColorListDto>> GetAllAsync()
+        {
+            var colors = await _colorRepository.GetAllAsync();
+            return _mapper.Map<List<ColorListDto>>(colors);
+        }
+
+        public async Task<ColorListDto?> GetByIdAsync(int id)
+        {
+            var color = await _colorRepository.GetAsync(x => x.Id == id);
+            return _mapper.Map<ColorListDto>(color);
+        }
+
+        public async Task<bool> UpdateAsync(ColorUpdateDto colorUpdateDto)
+        {
+            var existingColor = await _colorRepository.GetAsync(x => x.Id == colorUpdateDto.Id);
+            if (existingColor == null)
+            {
+                return false;
+            }
+            _mapper.Map(colorUpdateDto, existingColor);
+            await _colorRepository.UpdateAsync(existingColor);
+            return true;
+        }
+    }
+}
