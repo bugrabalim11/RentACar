617034f1c80776a78374bad9e1edc30aaf5ced64
bugrabalim11 <bugrablm11@gmail.com>
2026-07-13 20:39:51 +0300

    feat: Customer modülü için DTO sınıfları ve FluentValidation kuralları eklendi.
    - Müşteri işlemleri için CustomerAddDto, CustomerUpdateDto ve CustomerListDto sınıfları oluşturuldu.
    - Müşteri ekleme işleminde sisteme çöp veri girmesini engellemek adına CustomerAddDtoValidator ile doğrulama (Validation) kuralları yazıldı.
    - TC Kimlik No, E-Posta formatı ve İsim uzunlukları için katı güvenlik kuralları belirlendi.

---
+++ RentACar.Dtos/CustomerDtos/CustomerAddDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Dtos.CustomerDtos
+{
+    public class CustomerAddDto
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string NationalIdentity { get; set; } = null!;
+        public int DrivinglicenseYear { get; set; }
+    }
+}
---
+++ RentACar.Dtos/CustomerDtos/CustomerListDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Dtos.CustomerDtos
+{
+    public class CustomerListDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string NationalIdentity { get; set; } = null!;
+        public int DrivinglicenseYear { get; set; }
+    }
+}
