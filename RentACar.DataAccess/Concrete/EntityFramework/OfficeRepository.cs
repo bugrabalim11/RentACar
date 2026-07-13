9faf303f32df8e057717feb11ba4ec005b2e4493
bugrabalim11 <bugrablm11@gmail.com>
2026-07-13 18:59:47 +0300

    feat: Customer, Office ve Rental tabloları için özel Repository'ler oluşturuldu.
    - DataAccess katmanında ICustomerRepository, IOfficeRepository ve IRentalRepository arayüzleri eklendi.
    - Bu arayüzleri implemente eden ve temel Repository sınıfından miras alan CustomerRepository, OfficeRepository ve RentalRepository sınıfları oluşturuldu.
    - İleride gerekecek olan karmaşık Join/Include işlemleri için altyapı hazırlandı.

---
+++ RentACar.DataAccess/Concrete/EntityFramework/OfficeRepository.cs
@@ -0,0 +1,15 @@
+﻿using RentACar.DataAccess.Abstract;
+using RentACar.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.DataAccess.Concrete.EntityFramework
+{
+    public class OfficeRepository : Repository<Office>, IOfficeRepository
+    {
+        public OfficeRepository(RentACarContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ RentACar.DataAccess/Concrete/EntityFramework/RentalRepository.cs
@@ -0,0 +1,15 @@
+﻿using RentACar.DataAccess.Abstract;
+using RentACar.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.DataAccess.Concrete.EntityFramework
+{
+    public class RentalRepository : Repository<Rental>, IRentalRepository
+    {
+        public RentalRepository(RentACarContext context) : base(context)
+        {
+        }
+    }
+}
