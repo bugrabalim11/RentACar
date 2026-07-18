61c16fa828b0d08a98c0f5c9b0f712fcbf594045
bugrabalim11 <bugrablm11@gmail.com>
2026-07-18 16:32:35 +0300

    feat: İletişim mesajları (ContactMessageAddDto) için FluentValidation kuralları eklendi.

---
+++ RentACar.Business/ValidationRules/ContactMessageValidators/ContactMessageAddDtoValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using RentACar.Dtos.ContactMessageDtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Business.ValidationRules.ContactMessageValidators
+{
+    public class ContactMessageAddDtoValidator : AbstractValidator<ContactMessageAddDto>
+    {
+        public ContactMessageAddDtoValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().MinimumLength(2).WithMessage("İsim en az 2 karakter olmalıdır.");
+
+            RuleFor(x => x.Email).NotEmpty().WithMessage("E-posta boş geçilemez.")
+                .EmailAddress().WithMessage("Lütfen geçerli bir e-posta adresi giriniz.");
+
+            RuleFor(x => x.Subject).NotEmpty()
+                .MinimumLength(3).WithMessage("Konu en az 3 karakter olmalıdır.")
+                .MaximumLength(100).WithMessage("Konu en fazla 100 karakter olmalıdır.");
+
+            RuleFor(x => x.Message).NotEmpty().MinimumLength(10).WithMessage("Mesaj en az 10 karakter olmalıdır.");
+        }
+    }
+}
