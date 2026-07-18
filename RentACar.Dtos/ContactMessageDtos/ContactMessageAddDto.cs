db010a9c93878f35e838fc6459b218e7abac4bbd
bugrabalim11 <bugrablm11@gmail.com>
2026-07-18 16:20:59 +0300

    feat: İletişim mesajları (ContactMessage) için DTO'lar oluşturuldu.
    - Müşteriden veri almak için ContactMessageAddDto, verileri listelemek için ContactMessageListDto eklendi.
    - Veri bütünlüğünü (Data Integrity) korumak ve evrakta sahteciliği önlemek adına UpdateDto kasıtlı olarak oluşturulmadı.

---
+++ RentACar.Dtos/ContactMessageDtos/ContactMessageAddDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Dtos.ContactMessageDtos
+{
+    public class ContactMessageAddDto
+    {
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Subject { get; set; } = null!;
+        public string Message { get; set; } = null!;
+    }
+}
---
+++ RentACar.Dtos/ContactMessageDtos/ContactMessageListDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Dtos.ContactMessageDtos
+{
+    public class ContactMessageListDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Subject { get; set; } = null!;
+        public string Message { get; set; } = null!;
+        public DateTime SendDate { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
