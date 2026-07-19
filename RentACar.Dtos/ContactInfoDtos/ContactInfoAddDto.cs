feb4152cb1a10fea4aa7d6c4d15993d14952eef6
bugrabalim11 <bugrablm11@gmail.com>
2026-07-19 14:51:21 +0300

    feat:ContactInfo Dto'ları oluşturuldu.

---
+++ RentACar.Dtos/ContactInfoDtos/ContactInfoAddDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Dtos.ContactInfoDtos
+{
+    public class ContactInfoAddDto
+    {
+        public string Address { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
---
+++ RentACar.Dtos/ContactInfoDtos/ContactInfoListDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Dtos.ContactInfoDtos
+{
+    public class ContactInfoListDto
+    {
+        public int Id { get; set; }
+        public string Address { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Email { get; set; } = null!;
+    }
+}
