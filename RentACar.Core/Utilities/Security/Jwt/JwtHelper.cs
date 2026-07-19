d7226563187a12d6e928958bb0496653e6343faf
bugrabalim11 <bugrablm11@gmail.com>
2026-07-20 00:16:51 +0300

    feat: JWT altyapısı için gerekli paketler kuruldu ve JwtHelper tesisatı bağlandı.

---
+++ RentACar.Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -0,0 +1,29 @@
+﻿using RentACar.Core.Entities.Concrete;
+using Microsoft.Extensions.Configuration; // IConfiguration için bu kütüphaneyi (ampul ile) eklemelisin.
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Core.Utilities.Security.Jwt
+{
+    public class JwtHelper : ITokenHelper
+    {
+        // 1. Gizli depolarımız (Kablolarımız)
+        public IConfiguration Configuration { get; }
+        private TokenOptions _tokenOptions;
+
+
+        // 2. Constructor (Tesisatçının kabloyu taktığı an!)
+        public JwtHelper(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            // Kablodan (appsettings.json) gelen veriyi bizim TokenOptions şablonumuza dolduruyoruz.
+            _tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>() ?? new TokenOptions();
+        }
+
+        public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
