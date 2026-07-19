76fd0cac804929374d6b7dfd51a81022d261e39a
bugrabalim11 <bugrablm11@gmail.com>
2026-07-19 13:07:09 +0300

    feat: ContactMessagesController eklendi ve İletişim API uçları dışa açıldı.
    - GetAll, GetById, Add ve Delete metotları API'ye bağlandı.
    - ChangeIsReadStatus işlemi için özel HttpPatch route'u (şalter mantığı) Controller'a entegre edildi.

---
+++ RentACar.API/Controllers/ContactMessagesController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RentACar.Business.Abstract;
+using RentACar.Core.Utilities.Results;
+using RentACar.Dtos.ContactMessageDtos;
+
+namespace RentACar.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactMessagesController : ControllerBase
+    {
+        private readonly IContactMessageService _contactMessageService;
+
+        public ContactMessagesController(IContactMessageService contactMessageService)
+        {
+            _contactMessageService = contactMessageService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _contactMessageService.GetAllAsync();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _contactMessageService.GetByIdAsync(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ContactMessageAddDto contactMessageAddDto)
+        {
+            var result = await _contactMessageService.AddAsync(contactMessageAddDto);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _contactMessageService.DeleteAsync(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPatch("ChangeIsReadStatus/{id}")]
+        public async Task<IActionResult> ChangeIsReadStatusAsync(int id)
+        {
+            var result = await _contactMessageService.ChangeIsReadStatusAsync(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
