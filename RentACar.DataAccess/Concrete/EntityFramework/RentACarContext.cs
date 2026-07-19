fe649cea860ac3da212cfbbe0552ed3353dd4fa3
bugrabalim11 <bugrablm11@gmail.com>
2026-07-19 21:27:24 +0300

    feat: User entity'si oluşturuldu ve PostgreSQL veritabanına aktarıldı.
    - Güvenlik işlemleri (Auth) için User varlığı Id, FirstName, LastName, Email, PasswordHash, PasswordSalt ve Status alanlarıyla eklendi.
    - Null referans hatalarını (CS8618) önlemek için Array.Empty ve null-forgiving operatörleri kullanıldı.
    - RentACarContext üzerinden DbSet eklendi ve Add-Migration / Update-Database komutlarıyla veritabanı şeması güncellendi.

--- RentACar.DataAccess/Concrete/EntityFramework/RentACarContext.cs
+++ RentACar.DataAccess/Concrete/EntityFramework/RentACarContext.cs
@@ -21,6 +21,7 @@ namespace RentACar.DataAccess.Concrete.EntityFramework
         public DbSet<ContactInfo> ContactInfos { get; set; }
         public DbSet<Brand> Brands { get; set; }
         public DbSet<Color> Colors { get; set; }
+        public DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
---
+++ RentACar.Entities/Concrete/User.cs
@@ -0,0 +1,21 @@
+﻿using RentACar.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Entities.Concrete
+{
+    public class User : IEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+
+        // byte dizisi yani byte[] formatında olacak!
+        // Çünkü kriptografi algoritmaları metinlerle değil, baytlarla - 0 ve 1'lerle - çalışır.
+        public byte[] PasswordHash { get; set; } = Array.Empty<byte>();
+        public byte[] PasswordSalt { get; set; } = Array.Empty<byte>();
+        public bool Status { get; set; }
+    }
+}
