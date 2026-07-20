596110022a1e78715b9cd7efcfd09d710664c8ed
bugrabalim11 <bugrablm11@gmail.com>
2026-07-20 18:32:39 +0300

    feat: UserValidators eklendi.

---
+++ RentACar.Business/ValidationRules/UserValidators/UserAddDtoValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using RentACar.Dtos.UserDtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RentACar.Business.ValidationRules.UserValidators
+{
+    public class UserAddDtoValidator : AbstractValidator<UserAddDto>
+    {
+        public UserAddDtoValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("Kullanıcı ismi boş geçilemez.")
+                .MinimumLength(2).WithMessage("Kullanıcı ismi en az 2 karakter olmalıdır.");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("Kullanıcı soyismi boş geçilemez.")
+                .MinimumLength(2).WithMessage("Kullancı soyismi en az 2 karakter olmalıdır.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("E-posta adresi boş geçilemez.")
+                .EmailAddress().WithMessage("Lütfen geçerli bir e-posta adresi giriniz.");
+        }
+    }
+}
