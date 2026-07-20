bba6604ad45649514543258c088b46564634b7af
bugrabalim11 <bugrablm11@gmail.com>
2026-07-20 14:30:02 +0300

    feat: Kayıt (Register) ve Giriş (Login) işlemleri için FluentValidation kuralları eklendi.
    - Business katmanında AuthValidators klasörü oluşturuldu.
    - UserForRegisterDto için şifre gücü (Regex) ve e-posta formatı kontrolleri eklendi.
    - UserForLoginDto için temel boşluk (NotEmpty) kontrolleri sağlandı.
    - Clean Code prensipleri gereği kullanılmayan namespace referansları temizlendi.

---
+++ RentACar.Business/ValidationRules/AuthValidators/UserForLoginDtoValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using RentACar.Core.Entities.DTOs;
+
+namespace RentACar.Business.ValidationRules.AuthValidators
+{
+    public class UserForLoginDtoValidator : AbstractValidator<UserForLoginDto>
+    {
+        public UserForLoginDtoValidator()
+        {
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Lütfen e-posta adresinizi giriniz.");
+
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Lütfen şifrenizi giriniz.");
+        }
+    }
+}
---
+++ RentACar.Business/ValidationRules/AuthValidators/UserForRegisterDtoValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using RentACar.Core.Entities.DTOs;
+
+namespace RentACar.Business.ValidationRules.AuthValidators
+{
+    public class UserForRegisterDtoValidator : AbstractValidator<UserForRegisterDto>
+    {
+        public UserForRegisterDtoValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("Kullanıcı adı boş geçilemez.")
+                .MinimumLength(2).WithMessage("Kullanıcı adı en az 2 karakter olmalıdır.");
+
+            RuleFor(x => x.LastName).NotEmpty().WithMessage("Kullanıcı soyadı boş geçilemez.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("E-posta adresi boş geçilemez.")
+                .EmailAddress().WithMessage("Lütfen geçerli bir e-posta adresi giriniz.");
+
+            RuleFor(x=>x.Password)
+                .NotEmpty().WithMessage("Şifre alanı boş geçilemez.")
+                .MinimumLength(8).WithMessage("Şifre en az 8 karakter olmalıdır.")
+                .Matches("[A-Z]").WithMessage("Şifre en az bir büyük harf içermelidir.")
+                .Matches("[a-z]").WithMessage("Şifre en az bir küçük harf içermelidir.")
+                .Matches("[0-9]").WithMessage("Şifre en az bir rakam içermelidir.");
+        }
+    }
+}
