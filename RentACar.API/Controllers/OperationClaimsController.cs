79696cad7ecf1842e4f7d5f5292565fec3add33c
bugrabalim11 <bugrablm11@gmail.com>
2026-07-22 20:17:49 +0300

    feat: OperationClaimsController endpointleri kodlandı ve swagger teseti başarılı oldu.

---
+++ RentACar.API/Controllers/OperationClaimsController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RentACar.Business.Abstract;
+using RentACar.Core.Entities.DTOs.OperationClaimDtos;
+
+namespace RentACar.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationClaimsController : ControllerBase
+    {
+        private readonly IOperationClaimService _operationClaimService;
+
+        public OperationClaimsController(IOperationClaimService operationClaimService)
+        {
+            _operationClaimService = operationClaimService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _operationClaimService.GetAllAsync();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _operationClaimService.GetByIdAsync(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(OperationClaimAddDto operationClaimAddDto)
+        {
+            var result = await _operationClaimService.AddAsync(operationClaimAddDto);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(OperationClaimUpdateDto operationClaimUpdateDto)
+        {
+            var result = await _operationClaimService.UpdateAsync(operationClaimUpdateDto);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _operationClaimService.DeleteAsync(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
