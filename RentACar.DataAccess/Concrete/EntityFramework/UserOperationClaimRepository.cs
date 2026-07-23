4ed665d20675eb3ad02f6913407dfc181d2ed686
bugrabalim11 <bugrablm11@gmail.com>
2026-07-23 10:36:16 +0300

    feat: UserOperationClaim için DataAccess katmanı oluşturuldu.
    - Kullanıcılara rütbe/rol ataması yapabilmek için IUserOperationClaimRepository arayüzü eklendi.
    - Entity Framework altyapısını kullanan UserOperationClaimRepository somut sınıfı kodlandı.

---
+++ RentACar.DataAccess/Concrete/EntityFramework/UserOperationClaimRepository.cs
@@ -0,0 +1,12 @@
+﻿using RentACar.Core.Entities.Concrete;
+using RentACar.DataAccess.Abstract;
+
+namespace RentACar.DataAccess.Concrete.EntityFramework
+{
+    public class UserOperationClaimRepository : Repository<UserOperationClaim>, IUserOperationClaimRepository
+    {
+        public UserOperationClaimRepository(RentACarContext context) : base(context)
+        {
+        }
+    }
+}
