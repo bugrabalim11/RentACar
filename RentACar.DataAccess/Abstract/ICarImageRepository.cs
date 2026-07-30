c8fafc909b31db4907a3a9d67ee7880639085cf1
bugrabalim11 <bugrablm11@gmail.com>
2026-07-31 00:15:34 +0300

    feat(DAL): CarImage nesnesi için veri erişim katmanı arayüzü ve EF Core implementasyonu oluşturuldu.

---
+++ RentACar.DataAccess/Abstract/ICarImageRepository.cs
@@ -0,0 +1,8 @@
+﻿using RentACar.Entities.Concrete;
+
+namespace RentACar.DataAccess.Abstract
+{
+    public interface ICarImageRepository : IRepository<CarImage>
+    {
+    }
+}
---
+++ RentACar.DataAccess/Concrete/EntityFramework/CarImageRepository.cs
@@ -0,0 +1,12 @@
+﻿using RentACar.DataAccess.Abstract;
+using RentACar.Entities.Concrete;
+
+namespace RentACar.DataAccess.Concrete.EntityFramework
+{
+    public class CarImageRepository : Repository<CarImage>, ICarImageRepository
+    {
+        public CarImageRepository(RentACarContext context) : base(context)
+        {
+        }
+    }
+}
