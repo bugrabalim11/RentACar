f67367f74a357ffdaa295ae4a0feecaa32c6d212
bugrabalim11 <bugrablm11@gmail.com>
2026-07-31 20:25:40 +0300

    feat(Core): FileHelper altyapısı kuruldu ve Upload (dosya yükleme) yeteneği eklendi.

---
+++ RentACar.Core/Utilities/Helpers/FileHelper/FileHelperManager.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace RentACar.Core.Utilities.Helpers.FileHelper
+{
+    public class FileHelperManager : IFileHelper
+    {
+        public string? Upload(IFormFile file, string root)
+        {
+            if (file.Length > 0)
+            {
+                // 1. Dosya uzantısını alıyoruz (Örn: .jpg, .png)
+                string extension = Path.GetExtension(file.FileName);
+
+                // 2. Aynı isimde dosyalar çakışmasın diye benzersiz bir isim (GUID) üretiyoruz
+                string newFileName = Guid.NewGuid().ToString() + extension;
+
+                // 3. Eğer belirttiğimiz klasör yoksa, o klasörü oluşturuyoruz.
+                if (!Directory.Exists(root))
+                {
+                    Directory.CreateDirectory(root);
+                }
+
+                // 4. Klasör yolu ile yeni dosya adını birleştirip tam adresi çıkarıyoruz.
+                string imagePath = Path.Combine(root, newFileName);
+
+                using (FileStream fileStream = File.Create(imagePath))
+                {
+                    // Kargo bandı çalıştı!
+                    file.CopyTo(fileStream);
+                }
+
+                return imagePath;
+            }
+            return null;
+        }
+    }
+}
---
+++ RentACar.Core/Utilities/Helpers/FileHelper/IFileHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace RentACar.Core.Utilities.Helpers.FileHelper
+{
+    public interface IFileHelper
+    {
+        /// <summary>
+        /// Dışarıdan gelen dosyayı sunucuda belirtilen klasöre kaydeder ve yeni dosya yolunu döndürür.
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="root">Kaydedilecek klasörün fiziki yolu</param>
+        /// <returns></returns>
+        string? Upload(IFormFile file, string root);
+    }
+}
