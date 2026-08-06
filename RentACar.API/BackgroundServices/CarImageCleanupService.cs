aa599597840f532906c48f6d36348dbe7c3caec1
Buğra Balım <bugrablm11@gmail.com>
2026-08-06 10:44:39 +0300

    Gece bekçisi iskeleti ve otonom döngü oluşturuldu.

---
+++ RentACar.API/BackgroundServices/CarImageCleanupService.cs
@@ -0,0 +1,35 @@
+﻿namespace RentACar.API.BackgroundServices
+{
+    // BackgroundService'den miras alarak bu sınıfı "Ölümsüz bir Gece Bekçisine" çeviriyoruz.
+    public class CarImageCleanupService : BackgroundService
+    {
+        // Bekçimiz ölümsüz (Singleton) olduğu için ölümlü (Scoped) aşçılarla doğrudan çalışamaz.
+        // Bu yüzden ona aşçıları üretecek bir 'Vardiya Fabrikası' (Scope Factory) veriyoruz.
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public CarImageCleanupService(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
+
+        // ExecuteAsync: Bekçinin devriye gezdiği ana koridordur. Proje çalıştığı an burası tetiklenir.
+        // stoppingToken: Şalteri indirme (iptal) telsizidir.
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            // DİKKAT: Başında '!' var. Yani: "İptal sinyali GELMEDİĞİ sürece durmadan dön!"
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                // Fabrikayı çalıştırıp 1 gecelik, işi bitince hafızadan silinecek bir vardiya (Scope) yaratıyoruz.
+                using (var scope = _scopeFactory.CreateScope())
+                {
+                    // TODO: Birazdan buraya ICarImageService (Aşçı) çağrılacak ve resimler silinecek.
+
+                    // Bekçiyi uyku moduna alıyoruz. Şimdilik test için 1 dakika (FromMinutes(1)) uyuyor.
+                    // İşler bitince bunu 24 saate (FromHours(24)) çekeceğiz.
+                    await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+                }
+            }
+        }
+    }
+}
