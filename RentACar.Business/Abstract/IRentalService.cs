67624097d53022f9d3e7fdce01d3c5be954bb592
Buğra Balım <bugrablm11@gmail.com>
2026-08-20 11:55:11 +0300

    feat(rental-manager): Detay sayfası için ağır kargo paketi (RentalDetailDto) manager ve interface katmanlarına entegre edildi.

--- RentACar.Business/Abstract/IRentalService.cs
+++ RentACar.Business/Abstract/IRentalService.cs
@@ -6,9 +6,9 @@ namespace RentACar.Business.Abstract
     public interface IRentalService
     {
         Task<IDataResult<List<RentalListDto>>> GetAllAsync();
-        Task<IDataResult<RentalListDto>> GetByIdAsync(int id);
+        Task<IDataResult<RentalDetailDto>> GetByIdAsync(int id);
         Task<IDataResult<List<RentalListDto>>> GetAllByUserIdAsync(int userId);
-        Task<IDataResult<RentalListDto>> GetMyRentalByIdAsync(int rentalId, int userId);
+        Task<IDataResult<RentalDetailDto>> GetMyRentalByIdAsync(int rentalId, int userId);
         Task<IResult> CheckIfAnyRentalExistsByOfficeIdAsync(int officeId);
         Task<IResult> AddAsync(RentalAddDto rentalAddDto, int userId);
         Task<IResult> AddByAdminAsync(RentalAddByAdminDto rentalAddByAdminDto);
