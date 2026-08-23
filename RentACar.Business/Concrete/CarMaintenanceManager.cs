00e4b54eb5d7f48f20da26eedb693ade4fb856d9
Buğra Balım <bugrablm11@gmail.com>
2026-08-23 10:50:49 +0300

    feat(maintenance): Car maintenance AddAsync metodu, utc saat ayarı ve business rules eklendi.

--- RentACar.Business/Concrete/CarMaintenanceManager.cs
+++ RentACar.Business/Concrete/CarMaintenanceManager.cs
@@ -1,22 +1,46 @@
-﻿using RentACar.Business.Abstract;
+﻿using AutoMapper;
+using RentACar.Business.Abstract;
+using RentACar.Core.Utilities.Business;
 using RentACar.Core.Utilities.Results;
 using RentACar.DataAccess.Abstract;
 using RentACar.Dtos.CarMaintenanceDtos;
+using RentACar.Entities.Concrete;
 
 namespace RentACar.Business.Concrete
 {
     public class CarMaintenanceManager : ICarMaintenanceService
     {
         private readonly ICarMaintenanceRepository _carMaintenanceRepository;
+        private readonly IMapper _mapper;
+        private readonly ICarService _carService;
 
-        public CarMaintenanceManager(ICarMaintenanceRepository carMaintenanceRepository)
+        public CarMaintenanceManager(ICarMaintenanceRepository carMaintenanceRepository, IMapper mapper, ICarService carService)
         {
             _carMaintenanceRepository = carMaintenanceRepository;
+            _mapper = mapper;
+            _carService = carService;
         }
 
-        public Task<IResult> AddAsync(CarMaintenanceAddDto carMaintenanceAddDto)
+        public async Task<IResult> AddAsync(CarMaintenanceAddDto carMaintenanceAddDto)
         {
-            throw new NotImplementedException();
+            IResult? result = BusinessRules.Run(
+            await CheckIfCarExists(carMaintenanceAddDto.CarId),
+            await CheckIfCarIsAlreadyInMaintenance(carMaintenanceAddDto.CarId)
+            );
+            if (result != null)
+            {
+                return result;
+            }
+
+            var maintenance = _mapper.Map<CarMaintenance>(carMaintenanceAddDto);
+            // Saatleri ŞİMDİ Entity üzerinde UTC'ye çevir!
+            carMaintenanceAddDto.CheckInTime = DateTime.SpecifyKind(carMaintenanceAddDto.CheckInTime, DateTimeKind.Utc);
+            if (carMaintenanceAddDto.CheckOutTime.HasValue)
+            {
+                carMaintenanceAddDto.CheckOutTime = DateTime.SpecifyKind(carMaintenanceAddDto.CheckOutTime.Value, DateTimeKind.Utc);
+            }
+            await _carMaintenanceRepository.AddAsync(maintenance);
+            return new SuccessResult("Aracın tamir tarihleri başarıyla sisteme kaydedildi.");
         }
 
         public Task<IResult> DeleteAsync(int id)
@@ -37,6 +61,26 @@ namespace RentACar.Business.Concrete
         public Task<IResult> UpdateAsync(CarMaintenanceUpdateDto carMaintenanceUpdateDto)
         {
             throw new NotImplementedException();
+        }
+
+        private async Task<IResult> CheckIfCarIsAlreadyInMaintenance(int carId)
+        {
+            bool result = await _carMaintenanceRepository.AnyAsync(x => x.CarId == carId && x.CheckOutTime == null);
+            if (result)
+            {
+                return new ErrorResult("Bu araç şu an zaten bakımda!");
+            }
+            return new SuccessResult();
+        }
+
+        private async Task<IResult> CheckIfCarExists(int carId)
+        {
+            var result = await _carService.CheckIfCarExistsAsync(carId);
+            if (!result.Success)
+            {
+                return new ErrorResult("Böyle bir araç sistemde bulunamadı!");
+            }
+            return new SuccessResult();
         }
     }
 }
