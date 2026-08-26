415346044238ee67860790a676d1163e68924b01
Buğra Balım <bugrablm11@gmail.com>
2026-08-26 13:45:37 +0300

    feat(status): CarStatusManager içine kiralama ve sanayi çapraz tarih kontrolleri eklendi.

--- RentACar.Business/Abstract/ICarStatusService.cs
+++ RentACar.Business/Abstract/ICarStatusService.cs
@@ -1,6 +1,10 @@
-﻿namespace RentACar.Business.Abstract
+﻿using RentACar.Core.Utilities.Results;
+
+namespace RentACar.Business.Abstract
 {
     public interface ICarStatusService
     {
+        Task<IResult> CheckIfCarIsRentedAsync(int carId, DateTime startDate, DateTime? endDate);
+        Task<IResult> CheckIfCarIsInMaintenanceAsync(int carId, DateTime startDate, DateTime? endDate);
     }
 }
--- RentACar.Business/Concrete/CarStatusManager.cs
+++ RentACar.Business/Concrete/CarStatusManager.cs
@@ -1,4 +1,5 @@
 ﻿using RentACar.Business.Abstract;
+using RentACar.Core.Utilities.Results;
 using RentACar.DataAccess.Abstract;
 
 namespace RentACar.Business.Concrete
@@ -16,6 +17,27 @@ namespace RentACar.Business.Concrete
         {
             _rentalRepository = rentalRepository;
             _carMaintenanceRepository = carMaintenanceRepository;
+        }
+
+        public async Task<IResult> CheckIfCarIsInMaintenanceAsync(int carId, DateTime startDate, DateTime? endDate)
+        {
+            // (Yeni Başlangıç <= Eski Bitiş) VE (Yeni Bitiş >= Eski Başlangıç)
+            bool isMaintenance = await _carMaintenanceRepository.AnyAsync(x => x.CarId == carId && (x.CheckOutTime == null || startDate <= x.CheckOutTime) && (endDate == null || endDate >= x.CheckInTime));
+            if (isMaintenance)
+            {
+                return new ErrorResult("Bu araç seçilen tarihlerde meşguldür!");
+            }
+            return new SuccessResult();
+        }
+
+        public async Task<IResult> CheckIfCarIsRentedAsync(int carId, DateTime startDate, DateTime? endDate)
+        {
+            bool isRented = await _rentalRepository.AnyAsync(x => x.CarId == carId && (x.ReturnDate == null || startDate <= x.ReturnDate) && (endDate == null || endDate >= x.RentDate));
+            if (isRented)
+            {
+                return new ErrorResult("Bu araç seçilen tarihlerde meşguldür!");
+            }
+            return new SuccessResult();
         }
     }
 }
