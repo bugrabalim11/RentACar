c80068ea1671969521841cc09798671a05628f77
Buğra Balım <bugrablm11@gmail.com>
2026-08-26 18:16:35 +0300

    feat(findex): IFindexScoreService dublör servisi eklendi ve IoC kaydı yapıldı.

---
+++ RentACar.Business/Concrete/FindexScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using RentACar.Business.Abstract;
+using RentACar.Core.Utilities.Results;
+
+namespace RentACar.Business.Concrete
+{
+    public class FindexScoreManager : IFindexScoreService
+    {
+        private readonly IFindexScoreService _findexScoreService;
+
+        public FindexScoreManager(IFindexScoreService findexScoreService)
+        {
+            _findexScoreService = findexScoreService;
+        }
+
+        public int GetScoreByCustomerId(int customerId)
+        {
+            Random random = new Random();
+            int result = random.Next(0, 1901); // 1900 dahil olması için 1901 yazarız.
+            return result;
+        }
+    }
+}
