bbc1b693ff87164b9d5a171a6585e94068c87a4e
Buğra Balım <bugrablm11@gmail.com>
2026-08-27 14:03:31 +0300

    feat(admin): Admin Layout iskeleti kuruldu ve ve dashboard test sürüşü tamamlandı.

---
+++ RentACar.MVC/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RentACar.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")] // İşte bu yaka kartı!
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
