47608819da47c0ea550493a1038bdb59cea29cef
Buğra Balım <bugrablm11@gmail.com>
2026-08-29 20:22:44 +0300

    feat(admin): Marka silme işlemi ajax ve sweetalert ile entegre edildi.

--- RentACar.MVC/Areas/Admin/Controllers/BrandController.cs
+++ RentACar.MVC/Areas/Admin/Controllers/BrandController.cs
@@ -32,5 +32,18 @@ namespace RentACar.MVC.Areas.Admin.Controllers
             // API çağrısı başarısız olursa, sayfa patlamasın diye boş dönüyoruz.
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var responseMessage = await client.DeleteAsync($"https://localhost:7085/api/Brands/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return Json(new { success = true });
+            }
+            return Json(new { success = false, message = "API tarafında silme işlemi başarısız oldu!" });
+        }
     }
 }
