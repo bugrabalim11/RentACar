a28a4f3f133bacd1b9b041f7ee5b94e8478277c3
Buğra Balım <bugrablm11@gmail.com>
2026-08-31 11:55:39 +0300

    feat(admin-color): Color DTO' ları oluşturuldu ve Validation-Required dahil edildi.

---
+++ RentACar.MVC/Areas/Admin/Models/ColorDtos/ColorListDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RentACar.MVC.Areas.Admin.Models.ColorDtos
+{
+    public class ColorListDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ RentACar.MVC/Areas/Admin/Models/ColorDtos/GetByIdColorResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RentACar.MVC.Areas.Admin.Models.ColorDtos
+{
+    public class GetByIdColorResponseDto
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = null!;
+        public ColorUpdateDto Data { get; set; } = null!;
+    }
+}
