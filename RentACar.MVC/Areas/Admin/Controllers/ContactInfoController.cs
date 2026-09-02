200ed09dafac42259cb2f4de876a7f1275d88151
Buğra Balım <bugrablm11@gmail.com>
2026-09-02 11:30:01 +0300

    feat(admin-contactinfo): İletisim bilgisi ekleme (create) operasyonu ve gorunumu tamamlandi.

--- RentACar.MVC/Areas/Admin/Controllers/ContactInfoController.cs
+++ RentACar.MVC/Areas/Admin/Controllers/ContactInfoController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using RentACar.MVC.Areas.Admin.Models.ContactInfoDtos;
+using RentACar.MVC.Areas.Admin.Models.ErrorResponseDtos;
+using System.Text;
 
 namespace RentACar.MVC.Areas.Admin.Controllers
 {
@@ -45,6 +47,40 @@ namespace RentACar.MVC.Areas.Admin.Controllers
                 return Json(new { success = false, message = "Bu işlem için yetkiniz yok. Lütfen giriş yapın!" });
             }
             return Json(new { success = false, message = "Api tarafından silme işlemi başarısız oldu!" });
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ContactInfoCreateDto contactInfoCreateDto)
+        {
+            if (!ModelState.IsValid) { return View(contactInfoCreateDto); }
+            var client = _httpClientFactory.CreateClient("RentACarApi");
+
+            var jsonData = JsonConvert.SerializeObject(contactInfoCreateDto);
+            var stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("api/ContactInfos", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            if (responseMessage.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                ModelState.AddModelError(string.Empty, "Bu işlem için yetkiniz yok. Lütfen giriş yapın!");
+                return View(contactInfoCreateDto);
+            }
+
+            var errorJsonData = await responseMessage.Content.ReadAsStringAsync();
+            var errorData = JsonConvert.DeserializeObject<ErrorResponseDto>(errorJsonData);
+            if (errorData != null)
+            {
+                ModelState.AddModelError(string.Empty, errorData.Message);
+            }
+            return View(contactInfoCreateDto);
         }
     }
 }
