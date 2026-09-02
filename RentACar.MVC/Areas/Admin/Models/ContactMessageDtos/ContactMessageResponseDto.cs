180807b0de1722a8abd5b9cd82e648f04a12d945
Buğra Balım <bugrablm11@gmail.com>
2026-09-02 16:57:41 +0300

    feat(admin-contactmessage): İletişim mesajları listeleme DTO' ları eklendi.

---
+++ RentACar.MVC/Areas/Admin/Models/ContactMessageDtos/ContactMessageResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RentACar.MVC.Areas.Admin.Models.ContactMessageDtos
+{
+    public class ContactMessageResponseDto
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = null!;
+        public List<ContactMessageResultDto> Data { get; set; } = null!;
+    }
+}
---
+++ RentACar.MVC/Areas/Admin/Models/ContactMessageDtos/ContactMessageResultDto.cs
@@ -0,0 +1,13 @@
+﻿namespace RentACar.MVC.Areas.Admin.Models.ContactMessageDtos
+{
+    public class ContactMessageResultDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Subject { get; set; } = null!;
+        public string Message { get; set; } = null!;
+        public DateTime SendDate { get; set; }
+        public bool IsRead { get; set; }
+    }
+}
