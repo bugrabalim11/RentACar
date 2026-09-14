2ad35a18774eb1f4b4bf3ae0543fd9fe25f1884e
Buğra Balım <bugrablm11@gmail.com>
2026-09-14 18:08:35 +0300

    feat(admin-user): Kullanici geri getirme (Restore) metodu eklendi.
    - User/Index kullanciyi geri getir butonu ve JavaScript kodlari eklendi ve bazi UI degisiklikleri yapildi.

--- RentACar.MVC/Areas/Admin/Controllers/UserController.cs
+++ RentACar.MVC/Areas/Admin/Controllers/UserController.cs
@@ -49,5 +49,24 @@ namespace RentACar.MVC.Areas.Admin.Controllers
             }
             return Json(new { success = false, message = "Api tarafından silme işlemi başarısız oldu!" });
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Restore(int id)
+        {
+            var client = _httpClientFactory.CreateClient("RentACarApi");
+
+            // NOT: Patch işlemi kural gereği bir veri paketi (Body) bekler.
+            // Sadece ID ile işlem yaptığımız ve ekstra verimiz olmadığı için kuryenin eline boş bir kutu (StringContent) veriyoruz.
+            var responseMessage = await client.PatchAsync($"api/Users/restore/{id}", new StringContent(""));
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return Json(new { success = true });
+            }
+            if (responseMessage.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                return Json(new { success = false, message = "Bu işlem için yetkiniz yok. Lütfen giriş yapın!" });
+            }
+            return Json(new { success = false, message = "Api tarafından geri getirme işlemi başarısız oldu!" });
+        }
     }
 }
