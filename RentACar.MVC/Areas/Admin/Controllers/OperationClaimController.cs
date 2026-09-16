ece5d7e8f77798bc8d0249b01c9d2e00aab5a9a8
Buğra Balım <bugrablm11@gmail.com>
2026-09-16 18:29:35 +0300

    feat(admin-operationClaim): Controller acildi ve Index (HttpGet) ve gorunumu (View) tamamlandi.
    - OperationClaimResultDto'dan TODO aciklama satiri kaldirildi.
    - AdminSidebar'a Statuler butonu eklendi.

---
+++ RentACar.MVC/Areas/Admin/Controllers/OperationClaimController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACar.MVC.Areas.Admin.Models.OperationClaimDtos;
+using RentACar.MVC.Models.Responses;
+
+namespace RentACar.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class OperationClaimController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public OperationClaimController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient("RentACarApi");
+            var responseMessage = await client.GetAsync("api/OperationClaims");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var responseBox = JsonConvert.DeserializeObject<ResponseModel<List<OperationClaimResultDto>>>(jsonData);
+                if (responseBox != null && responseBox.Data != null)
+                {
+                    return View(responseBox.Data);
+                }
+            }
+            return View(new List<OperationClaimResultDto>());
+        }
+    }
+}
--- RentACar.MVC/Areas/Admin/Models/OperationClaimDtos/OperationClaimResultDto.cs
+++ RentACar.MVC/Areas/Admin/Models/OperationClaimDtos/OperationClaimResultDto.cs
@@ -1,6 +1,5 @@
 ﻿namespace RentACar.MVC.Areas.Admin.Models.OperationClaimDtos
 {
-    // TODO: BUNU USERSTA ROLLERİ LİSTELEMEK İÇİN AÇTIK DEVAMI İLERDE KODLANACAK
     public class OperationClaimResultDto
     {
         public int Id { get; set; }
