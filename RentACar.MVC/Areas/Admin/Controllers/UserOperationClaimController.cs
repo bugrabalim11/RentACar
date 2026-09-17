45572e12d58eb501612a4cfad5e3761b5533d856
Buğra Balım <bugrablm11@gmail.com>
2026-09-17 14:26:37 +0300

    feat(admin-useroperationclaim): Kullanici rutbe listeleme (Index) metodu ve view'u tamamlandi.
    - MVC: API'den verileri karsilamak icin UserOperationClaimDetailDto olusturuldu.
    - MVC: Atama ekrani (Create) icin gerekli ViewModel ve Interface altyapilari hazirlandi.
    - MVC: Admin Sidebar'a "Kullanici Statuleri" butonu eklendi.

---
+++ RentACar.MVC/Areas/Admin/Controllers/UserOperationClaimController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RentACar.MVC.Areas.Admin.Models.UserOperationClaimDtos;
+using RentACar.MVC.Models.Responses;
+
+namespace RentACar.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserOperationClaimController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public UserOperationClaimController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient("RentACarApi");
+
+            var responseMessage = await client.GetAsync("api/UserOperationClaims/details");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var responseBox = JsonConvert.DeserializeObject<ResponseModel<List<UserOperationClaimDetailDto>>>(jsonData);
+                if (responseBox != null && responseBox.Data != null)
+                {
+                    return View(responseBox.Data);
+                }
+            }
+            return View(new List<UserOperationClaimDetailDto>());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var client = _httpClientFactory.CreateClient("RentACarApi");
+
+            var responseMessage = await client.DeleteAsync($"api/UserOperationClaims/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return Json(new { success = true });
+            }
+            if (responseMessage.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            {
+                return Json(new { success = false, message = "Bu işlem için yetkiniz yok. Lütfen giriş yapın!" });
+            }
+
+            return Json(new { success = false, message = "API tarafında silme işlemi başarısız oldu!" });
+        }
+    }
+}
---
+++ RentACar.MVC/Areas/Admin/Models/UserOperationClaimDtos/UserOperationClaimCreateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using RentACar.MVC.Areas.Admin.Models.OperationClaimDtos;
+using RentACar.MVC.Areas.Admin.Models.UserDtos;
+using RentACar.MVC.Models.Interfaces;
+
+namespace RentACar.MVC.Areas.Admin.Models.UserOperationClaimDtos
+{
+    public class UserOperationClaimCreateViewModel : IUserOperationClaimDropdownsViewModel
+    {
+        public List<UserResultForAdminDto> Users { get; set; } = null!;
+        public List<OperationClaimResultDto> Roles { get; set; } = null!;
+    }
+}
